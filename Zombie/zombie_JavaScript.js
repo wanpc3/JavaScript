eb0754a64b2aa9963a0537018ed7318dbb793bcc
Wan Ilhan Idriss <63439253+wanpc3@users.noreply.github.com>
2023-03-01 17:59:41 +0800

    Add files via upload

---
+++ Zombie/zombie_JavaScript.js
@@ -0,0 +1,8 @@
+//Let's make zombie death or alive
+function death(){
+	document.getElementById('zombie').src = 'img/death.png'
+}
+
+function respawn(){
+	document.getElementById('zombie').src = 'img/zombie.png'
+}
